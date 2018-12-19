656c835df148149bcb5a005c613b842120812007
Artdex Anc Cognoscis Tech <anc@Artdexs-iMac.local>
2018-12-19 20:43:02 +0530

    url pointed to sit server

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -13,16 +13,13 @@ import SdkKhata
 
 class ViewController: UIViewController,SendFPSDKResponseDelegate {
     
+    
+    
         
     @IBOutlet weak var applyBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(self.methodOfReceivedNotification(notification:)), name: Notification.Name("PayUResponse"), object: nil)
-        
-        
     }
 
     @IBAction func openKhataApp(_ sender: Any) {
@@ -31,63 +28,28 @@ class ViewController: UIViewController,SendFPSDKResponseDelegate {
         if let viewController = UIStoryboard(name: "FPApp", bundle: bundel).instantiateViewController(withIdentifier: "KhataVC") as? KhataViewController {
             
             viewController.sendFPSDKResponseDelegate = self
-            //viewController.mobileNumber = "8888888888"
-            
-            viewController.mobileNumber = "9029344445"
+            viewController.mobileNumber = "9819064110"
+            //viewController.mobileNumber = "9029344445"
             viewController.emailID = "testacc0990@gmail.com"
             viewController.zipcode = ""
             viewController.tokenId = ""
             viewController.DOB = "01/01/1990"
+            viewController.mandateStatus = "editMandate"
             self.navigationController?.pushViewController(viewController, animated: true)
         }
     }
     
     
-    @objc func methodOfReceivedNotification(notification: Notification){
-        //Take Action on Notification
-        
-        print(notification)
-        
-        let alert = UIAlertController(title: "Response", message: "\(notification.object!)" , preferredStyle: UIAlertControllerStyle.alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
-        
-        var merchantHash = String()
-        var strConvertedRespone = "\(notification.object!)"
-        
-        // var jsonResult  = try JSONSerialization.jsonObject(with: notification.object!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-        
-        
-        var JSON : NSDictionary = try! JSONSerialization.jsonObject(with: strConvertedRespone.data(using: String.Encoding.utf8)!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-        
-        if ((JSON.object(forKey: "status") as! String == "success"))
-        {
-            var cardToken = String()
-            print("The transaction is successful")
-            if (JSON.object(forKey: "cardToken")  != nil)
-            {
-                cardToken =  JSON.object(forKey: "cardToken") as! String
-                
-                if (JSON.object(forKey: "card_merchant_param") != nil)
-                {
-                    merchantHash = JSON.object(forKey: "card_merchant_param") as! String
-                    
-                }
-            }
-            
-            
-            
-        }
-        
-        self.present(alert, animated: true, completion: nil)
-    }
     
     
-    func sendResponse(sanctionAmount: Int, LAN: String,status:String,CIF:String) {
+    
+    func sendResponse(sanctionAmount: Int, LAN: String,status:String,CIF:String,mandateId:String) {
         print("Main APP")
         print("SanctionAmount : \(sanctionAmount)")
         print("LAN ID : \(LAN)")
         print("Status : \(status)")
         print("CIF : \(CIF)")
+        print("mandateId : \(mandateId)")
         self.applyBtn.setTitle(status, for: .normal)
     }
     
